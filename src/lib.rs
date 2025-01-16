8aa1d2a877cadb2a4e4983811a9cc8a8096e64c9
lesguillemets <kgkacko+github@gmail.com>
2025-01-16 16:16:42 +0900

    split loading

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,72 @@
 #![feature(let_chains)]
 
+use opencv::error;
 use opencv::prelude::*;
 use opencv::videoio::{VideoCapture, CAP_PROP_FRAME_COUNT};
+use std::path::Path;
 use std::process::ExitCode;
 
-pub fn load(f: &str) -> ExitCode {
-    if let Ok(mut vc) = VideoCapture::from_file(f, 0) {
-        if let Ok(frame_count) = vc.get(CAP_PROP_FRAME_COUNT)
-            && frame_count > 0.0
-        {
+pub fn do_load(f: &str) -> ExitCode {
+    match load_video(f) {
+        Ok((mut vc, frame_count)) => {
             eprintln!("ready to process video with {frame_count} frames");
             let frames = find_the_frames(&mut vc);
             ExitCode::SUCCESS
-        } else {
+        }
+        Err(LoadVideoError::OpenCVError(oce)) => {
+            eprintln!("Error on opencv: {:?}", oce);
+            ExitCode::FAILURE
+        }
+        Err(LoadVideoError::NoFrameError) => {
             eprintln!(
                 "error: CAP_PROP_FRAME_COUNT didn't return positive number; maybe not a video"
             );
             ExitCode::FAILURE
         }
+        Err(LoadVideoError::FileNotFoundError) => {
+            eprintln!("file not found");
+            ExitCode::FAILURE
+        }
+    }
+}
+
+#[derive(Debug)]
+/// OpenCVに起因するエラー，
+/// get(CAP_PROP_FRAME_COUNT) が0 （多分動画じゃない），
+/// ファイルが見つからない
+pub enum LoadVideoError {
+    OpenCVError(opencv::error::Error),
+    NoFrameError,
+    FileNotFoundError,
+}
+
+impl From<opencv::error::Error> for LoadVideoError {
+    fn from(err: opencv::error::Error) -> LoadVideoError {
+        LoadVideoError::OpenCVError(err)
+    }
+}
+
+/// file is given as a &str. Returns `([opencv::videoio::VideoCapture], usize)`
+/// where usize denotes for the frame count for the video.
+pub fn load_video(f: &str) -> Result<(VideoCapture, usize), LoadVideoError> {
+    if !Path::new(&f).is_file() {
+        return Err(LoadVideoError::FileNotFoundError);
+    }
+    let vc = VideoCapture::from_file(f, 0)?;
+    let frame_count = vc.get(CAP_PROP_FRAME_COUNT)?;
+    if frame_count > 0.0 {
+        Ok((vc, frame_count as usize))
     } else {
-        eprintln!("error reading file: probably file not found");
-        ExitCode::FAILURE
+        Err(LoadVideoError::NoFrameError)
     }
 }
 
 fn find_the_frames(vc: &mut VideoCapture) -> Vec<(usize, usize)> {
     let mut frame = Mat::default();
-    while let Ok(b) = vc.read(&mut frame)
-        && b
-    {
-        println!("{frame:?}");
-    }
+    // while let Ok(b) = vc.read(&mut frame)
+    //     && b
+    // {
+    //     println!("{frame:?}");
+    // }
     vec![]
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use ikfm2502timeit::load;
+use ikfm2502timeit::do_load;
 use std::process::ExitCode;
 
 #[derive(Parser, Debug)]
@@ -13,5 +13,5 @@ struct Args {
 fn main() -> ExitCode {
     let args = Args::parse();
     println!("{args:?}");
-    load(&args.file)
+    do_load(&args.file)
 }
