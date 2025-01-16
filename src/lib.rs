50ce9795306cea55c76e898db4b53758b2c0efb6
lesguillemets <kgkacko+github@gmail.com>
2025-01-16 11:47:01 +0900

    exit codes

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,24 @@
+#![feature(let_chains)]
+
 use opencv::prelude::*;
 use opencv::videoio::{VideoCapture, CAP_PROP_FRAME_COUNT};
+use std::process::ExitCode;
 
-pub fn load(f: &str) {
+pub fn load(f: &str) -> ExitCode {
     if let Ok(vc) = VideoCapture::from_file(f, 0) {
-        println!(
-            "ready to process video with {:?} frames",
-            vc.get(CAP_PROP_FRAME_COUNT)
-        );
+        if let Ok(frame_count) = vc.get(CAP_PROP_FRAME_COUNT)
+            && frame_count > 0.0
+        {
+            eprintln!("ready to process video with {frame_count} frames");
+            ExitCode::SUCCESS
+        } else {
+            eprintln!(
+                "error: CAP_PROP_FRAME_COUNT didn't return positive number; maybe not a video"
+            );
+            ExitCode::FAILURE
+        }
     } else {
-        println!("failed to read the video?");
+        eprintln!("error reading file: probably file not found");
+        ExitCode::FAILURE
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
-
 use ikfm2502timeit::load;
+use std::process::ExitCode;
 
 #[derive(Parser, Debug)]
 #[command(version)]
@@ -10,8 +10,8 @@ struct Args {
     file: String,
 }
 
-fn main() {
+fn main() -> ExitCode {
     let args = Args::parse();
     println!("{args:?}");
-    load(&args.file);
+    load(&args.file)
 }
