6df5b0ad1763ef907008de3c765b92147821ffb5
lesguillemets <kgkacko+github@gmail.com>
2025-01-16 12:54:17 +0900

    stash

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,12 @@ use opencv::videoio::{VideoCapture, CAP_PROP_FRAME_COUNT};
 use std::process::ExitCode;
 
 pub fn load(f: &str) -> ExitCode {
-    if let Ok(vc) = VideoCapture::from_file(f, 0) {
+    if let Ok(mut vc) = VideoCapture::from_file(f, 0) {
         if let Ok(frame_count) = vc.get(CAP_PROP_FRAME_COUNT)
             && frame_count > 0.0
         {
             eprintln!("ready to process video with {frame_count} frames");
+            let frames = find_the_frames(&mut vc);
             ExitCode::SUCCESS
         } else {
             eprintln!(
@@ -21,4 +22,14 @@ pub fn load(f: &str) -> ExitCode {
         eprintln!("error reading file: probably file not found");
         ExitCode::FAILURE
     }
+}
+
+fn find_the_frames(vc: &mut VideoCapture) -> Vec<(usize, usize)> {
+    let mut frame = Mat::default();
+    while let Ok(b) = vc.read(&mut frame)
+        && b
+    {
+        println!("{frame:?}");
+    }
+    vec![]
 }
